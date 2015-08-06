9fb2f3f3158e6b12d3ad0ba80d3056d24e102eef
Petr Marek <ermrk@seznam.cz>
2015-08-06 15:04:02 +0200

    Email is no longer needed for submitting feedback

--- www/js/feedbackHandler.js
+++ www/js/feedbackHandler.js
@@ -16,7 +16,6 @@ var feedbackButtons;
 
 //click on submit button
 $(document).on('click', '#form-submit', function () {
-    if (isValidEmailAddress($('#email').val())) {
         var email = $('#email').val();
         if (supports_html5_storage()) {
             localStorage.setItem("email", email);
@@ -26,10 +25,6 @@ $(document).on('click', '#form-submit', function () {
         } else {
             sendWithIncorrect(email);
         }
-    }
-    else {
-        alert("Wrong email address");
-    }
 });
 
 //send if answer was correct
@@ -159,12 +154,6 @@ function sendFeedback(email, question, ea1, ea2, ea3, ea4, ea5, ea6, mca) {
     LMca + "=" + Vmca +
     SUBMIT_REF);
     $.post(submitURL);
-}
-
-//check valid email address
-function isValidEmailAddress(emailAddress) {
-    var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
-    return pattern.test(emailAddress);
 }
 
 //restores feedback to default state
