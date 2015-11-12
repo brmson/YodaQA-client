95aa454e2a84d094885a158b76710a47302348c8
ermrk <ermrk@seznam.cz>
2015-11-12 13:31:26 +0100

    Generated concepts can be used as artificial concepts in next search.

--- js/artificialConceptHandler.js
+++ js/artificialConceptHandler.js
@@ -2,6 +2,11 @@
  * Created by Petr Marek on 29.10.2015.
  */
 
+var conceptButtons;
+
+var SELECTED = true;
+var NONSELECTED = false;
+
 //click on add more artificial concept button
 $(document).on('click', '#moreConceptButton', function (e) {
     var numberOfArtificialConcepts=$("#numberOfConcepts").val();
@@ -51,4 +56,40 @@ function showEraseArtificialConceptButton(){
 
 function hideEraseArtificialConceptButton(){
     $("#lessConceptButton").remove();
+}
+
+// shows button for choosing concepts near generated concepts
+function showChooseConceptButton(numberOfGeneratedConcepts) {
+    conceptButtons = [];
+    for (var i = 1; i <= numberOfGeneratedConcepts; i++) {
+        createChooseConceptButton(i);
+    }
+}
+
+//creates button for showing concept
+function createChooseConceptButton(i) {
+    conceptButtons[i] = NONSELECTED;
+
+    var conceptButton = '<button class="ui-btn ui-mini ui-corner-all ui-icon-myapp-unchecked ui-btn-icon-left ui-btn-inline ui-nodisc-icon" id="conceptButton' + i + '">Selected</button>';
+    $("#conceptButtonArea" + i).append(conceptButton);
+    $('#conceptButtonArea' + i).on('click', function (e) {
+        clickActionCorrect(i);
+        e.preventDefault();
+        e.stopPropagation();
+        e.stopImmediatePropagation();
+    });
+    $("#conceptButtonArea" + i).parent().css("overflow","visible");
+}
+
+//click function on correct button
+function clickActionCorrect(i) {
+    if (conceptButtons[i] == NONSELECTED) {
+        conceptButtons[i] = SELECTED;
+        $('#conceptButton' + i).addClass("ui-icon-myapp-checked");
+        $("#conceptButton" + i).removeClass('ui-icon-myapp-unchecked');
+    } else {
+        conceptButtons[i] = NONSELECTED;
+        $('#conceptButton' + i).addClass("ui-icon-myapp-unchecked");
+        $("#conceptButton" + i).removeClass('ui-icon-myapp-checked');
+    }
 }
