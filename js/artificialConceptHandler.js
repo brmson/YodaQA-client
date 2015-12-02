4a489f089298dab9538cda6046b1b09b282bbe1a
ermrk <ermrk@seznam.cz>
2015-12-02 11:27:26 +0100

    Deleted commented part of code in index.html, deleted unused methods in artificialConceptHandler.js

--- js/artificialConceptHandler.js
+++ js/artificialConceptHandler.js
@@ -1,62 +1,12 @@
 /**
  * Created by Petr Marek on 29.10.2015.
+ * This class shows and handles clicks on buttons of question concepts.
  */
 
 var conceptButtons;
 
 var SELECTED = true;
 var NONSELECTED = false;
-
-//click on add more artificial concept button
-$(document).on('click', '#moreConceptButton', function (e) {
-    var numberOfArtificialConcepts=$("#numberOfConcepts").val();
-    numberOfArtificialConcepts++;
-    addInputForArtificialConcepts(numberOfArtificialConcepts);
-    if (numberOfArtificialConcepts==2){
-        showEraseArtificialConceptButton()
-    }
-    $("#numberOfConcepts").val(numberOfArtificialConcepts);
-});
-
-//click on erase last artificial concept button
-$(document).on('click', '#lessConceptButton', function (e) {
-    var numberOfArtificialConcepts=$("#numberOfConcepts").val();
-    if (numberOfArtificialConcepts>1){
-        erraseLastInputForArtificialConcepts(numberOfArtificialConcepts);
-        numberOfArtificialConcepts--;
-        $("#numberOfConcepts").val(numberOfArtificialConcepts);
-        if (numberOfArtificialConcepts==1){
-            hideEraseArtificialConceptButton();
-        }
-    }
-
-});
-
-function addInputForArtificialConcepts(numberOfArtificialConcepts){
-    var container = $("#artificialConceptsBlock");
-    var toAppend='<input type="text" name="fullLabel'+numberOfArtificialConcepts+'" id="fullLabel'+numberOfArtificialConcepts+'" ' +
-        'placeholder="Concept Label" > ' +
-        '<input type="number" name="pageID'+numberOfArtificialConcepts+'" id="pageID'+numberOfArtificialConcepts+'" placeholder="Page id">'
-    container.append(toAppend);
-    $("#fullLabel"+numberOfArtificialConcepts).textinput();
-    $("#pageID"+numberOfArtificialConcepts).textinput();
-}
-
-function erraseLastInputForArtificialConcepts(numberOfArtificialConcepts){
-    $("#fullLabel"+numberOfArtificialConcepts).remove();
-    $("#pageID"+numberOfArtificialConcepts).remove();
-}
-
-function showEraseArtificialConceptButton(){
-    var container = $("#artificialConceptButtonBlock");
-    var toPrepend='<input type="button" name="lessConceptButton" id="lessConceptButton" value="Erase concept">'
-    container.prepend(toPrepend);
-    $("#lessConceptButton").button();
-}
-
-function hideEraseArtificialConceptButton(){
-    $("#lessConceptButton").remove();
-}
 
 // shows button for choosing concepts near generated concepts
 function showChooseConceptButton(numberOfGeneratedConcepts) {
